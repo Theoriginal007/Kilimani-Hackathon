09c1ecc144a004b31d109b6d18492c63b57e6167
davieongora <ongoradavid5@gmail.com>
2024-07-31 23:17:29 +0300

    push

--- src/views/Index.js
+++ src/views/Index.js
@@ -78,6 +78,7 @@ export default function Index() {
         </div>
         <div className="container mx-auto">
           <div className="flex flex-wrap items-center">
+            
             <div className="w-10/12 md:w-6/12 lg:w-4/12 px-12 md:px-4 mr-auto ml-auto -mt-32">
               <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded-lg bg-lightBlue-500">
                 <img
